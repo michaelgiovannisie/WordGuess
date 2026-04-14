bdf1518d019790f2094ac0b01dd55d4d36ac8d76
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-04-13 22:23:17 -0400

    adding core methods per instruction as requested

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -10,42 +10,105 @@ import java.util.Scanner;
 
 public class WordGuess {
 
-    private final String secretWord = "Liverpool";
+    private final char[] secretWord = "Liverpool".toCharArray();
     private char[] guessedLetters;
-    private int remainingAttempts = secretWord.length();
+    private int remainingAttempts = secretWord.length;\
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Word Guess!");
+        Scanner scanner = new Scanner(System.in);
         WordGuess game = new WordGuess();
-        game.play(new Scanner(System.in));
+        game.runGame(scanner);
     }
 
-    public void play(Scanner scanner) {
-        guessedLetters = new char[secretWord.length()];
-        for (int i = 0; i < guessedLetters.length; i++) {
-            guessedLetters[i] = '_';
-        }
-        System.out.println(guessedLetters);
+    public void runGame(Scanner scanner) {
 
         while (remainingAttempts > 0) {
             System.out.println(secretWord);
-            System.out.print("Enter a single character: ");
-            String input = scanner.nextLine().toUpperCase();
 
-            if(input.length() != 1) {
-                System.out.println("Invalid input. Please enter one character");
-                continue;
-            }
-
-            char guess = input.charAt(0);
-
-            if(!((guess >= 'a' && guess <= 'z') || (guess >= 'A' && guess <= 'Z'))) {
-                System.out.println("Invalid input. Please enter one character");
-                continue;
-            }
 
             remainingAttempts--;
         }
     }
 
+    public void announceGame() {
+        System.out.println("Welcome to Michael's WordGuess.");
+    }
+
+    public void gameOver() {
+        System.out.println("Game Over :( .");
+    }
+
+    public void initializeGameState() {
+        guessedLetters = new char[secretWord.length];
+        for (int i = 0; i < guessedLetters.length; i++) {
+            guessedLetters[i] = '_';
+        }
+    }
+
+    public char getNextGuess(Scanner scanner) {
+        while (true){
+            System.out.print("Enter a single character: ");
+            String input = scanner.nextLine().toUpperCase();
+            if(input.length() != 1) {
+                System.out.println("Invalid input. Please enter one character");
+                continue;
+            }
+            char guess = input.charAt(0);
+            if(!((guess >= 'a' && guess <= 'z') || (guess >= 'A' && guess <= 'Z'))) {
+                System.out.println("Invalid input. Please enter one character");
+                continue;
+            }
+            return guess;
+        }
+    }
+
+    public boolean isWordGuessed() {
+        for(int i = 0;i < guessedLetters.length;i++) {
+            if(guessedLetters[i] == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean askToPlayAgain(Scanner scanner) {
+        while (true){
+            System.out.print("Do you want to play again? (Yes/No)");
+            String input = scanner.nextLine().toLowerCase();
+            if(input.equals("yes") || input.equals("y")) {
+                return true;
+            } else if(input.equals("no") || input.equals("n")) {
+                return false;
+            } else {
+                System.out.println("Invalid input.");
+            }
+        }
+    }
+
+    public void printCurrentState(){
+        for (int i = 0; i < guessedLetters.length; i++) {
+            System.out.print(guessedLetters[i] + " ");
+            }
+        System.out.println();
+        System.out.println("You have " + remainingAttempts + " remaining attempts");
+    }
+
+    public void process(char guess) {
+        for(int i = 0;i < secretWord.length;i++) {
+            if(secretWord[i] == guess) {
+                guessedLetters[i] = guess;
+            }
+        }
+    }
+
+    public void playerWon() {
+        System.out.println("What a Legend!");
+    }
+
+    public void playerLost() {
+        System.out.println("Loser.");
+    }
+
 }
+
+
